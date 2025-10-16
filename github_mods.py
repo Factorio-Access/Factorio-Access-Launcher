8c648e51c43a8c75452675960c4d233638d90b4f
Gweneph <12696697+EphDoering@users.noreply.github.com>
2025-10-16 17:23:49 -0400

    swithced quotes for 3.11 compatability.

--- github_mods.py
+++ github_mods.py
@@ -15,18 +15,22 @@ opener.addheaders = [
     )
 ]
 
-def get_json(url:str):
+
+def get_json(url: str):
     with opener.open(url) as fp:
         return json.load(fp)
 
+
 BASE = "https://api.github.com/repos/Factorio-Access/FactorioAccess/"
 
-def get_tags(repo:str,user = "Factorio-Access"):
+
+def get_tags(repo: str, user="Factorio-Access"):
     return get_json(f"https://api.github.com/repos/{user}/{repo}/tags")
 
-def update_if_needed(current_version:str, name:str,user = "Factorio-Access"):
+
+def update_if_needed(current_version: str, name: str, user="Factorio-Access"):
     try:
-        tags = get_tags(name,user)
+        tags = get_tags(name, user)
         if not tags:
             print(f"No tags found for {name}")
             return None
@@ -34,23 +38,24 @@ def update_if_needed(current_version:str, name:str,user = "Factorio-Access"):
         if latest == current_version:
             return None
         print(f"Updating {name} to {latest}")
-        download(tags[0]["zipball_url"], MODS / f"{name}_{latest.strip(" v")}.zip")
+        download(tags[0]["zipball_url"], MODS / f"{name}_{latest.strip(' v')}.zip")
     except urllib.error.URLError:
         print(f"Network Error updating: {name}")
         return None
     return latest
 
+
 def update_all():
     for _ in range(2):
         try:
-            with open(MODS / "git_versions.txt","r+",encoding='utf8') as fp:
+            with open(MODS / "git_versions.txt", "r+", encoding="utf8") as fp:
                 new_file_contents = []
                 for line in fp:
                     parts = line.strip().split()
                     if len(parts) != 2:
                         continue
                     name, version = parts
-                    new_version = update_if_needed(version,name)
+                    new_version = update_if_needed(version, name)
                     if new_version:
                         version = new_version
                     new_file_contents.append(f"{name} {version}\n")
@@ -58,11 +63,12 @@ def update_all():
                 fp.truncate(0)
                 fp.writelines(new_file_contents)
         except FileNotFoundError:
-            with open(MODS / "git_versions.txt","w") as fp:
+            with open(MODS / "git_versions.txt", "w") as fp:
                 fp.write("FactorioAccess none\n")
                 fp.write("Kruise_Kontrol_Remote none\n")
         else:
             break
+
 
 if __name__ == "__main__":
     update_all()
